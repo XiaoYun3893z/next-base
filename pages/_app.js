6de65b7097eea7cd89c5e9b2364a16365383eaff
Yun <159111847+XiaoYun3893z@users.noreply.github.com>
2024-04-17 16:33:07 +0800

    css(引入)

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 // import '@/styles/globals.css'
 // next中只有_app.js可以引入全域樣式，其他頁面不行
-import '@/styles/product-table.css'
+// import '@/styles/product-table.css'
 
 export default function MyApp({ Component, pageProps }) {
   // Use the layout defined at the page level, if available
