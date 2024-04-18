7aecd8528b99ea29ee2f4f4be41e281411c04b96
Yun <159111847+XiaoYun3893z@users.noreply.github.com>
2024-04-18 15:21:47 +0800

    元件拆分組合-加入收藏-BookItem

---
+++ components/book-list/index.js
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+// 範例資料
+import data from '@/data/books.json'
+import BookItem from './book-item'
+
+export default function BookList() {
+  // 設定到初始狀態前，先擴增一個代表是否有加入收藏的屬性fav(布林，預設為false)
+  const initState = data.map((v, i) => {
+    return { ...v, fav: false }
+  })
+
+  const [books, setBooks] = useState(initState)
+
+  // 處理fav切換布林的函式
+  const handleToggleFav = (isbn) => {
+    // 1 2 展開每個成員
+    const nextBooks = books.map((v, i) => {
+      // 如果符合條件(isbn相等傳入isbn)，屬性fav的值邏輯反相(true=>false, false=>true)
+      if (v.isbn === isbn) return { ...v, fav: !v.fav }
+      // 否則回傳原本物件
+      else return v
+    })
+    // 3
+    setBooks(nextBooks)
+  }
+
+  return (
+    <>
+      <h1>書籍清單</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>ISBN</th>
+            <th>title</th>
+            <th>author</th>
+            <th>加入收藏</th>
+          </tr>
+        </thead>
+        <tbody>
+          {books.map((book) => {
+            return (
+              <BookItem
+                key={book.isbn}
+                book={book}
+                handleToggleFav={handleToggleFav}
+              />
+            )
+          })}
+        </tbody>
+      </table>
+    </>
+  )
+}
