807afb4cb668da01cbedc3592d126d81526256e8
Yun <159111847+XiaoYun3893z@users.noreply.github.com>
2024-04-25 09:24:27 +0800

    refs展示範例

--- components/refs-demo/input-refs.js
+++ components/refs-demo/input-refs.js
@@ -1,15 +1,19 @@
 import { useRef } from 'react'
 
 export default function InputRefs() {
-  // 1. 初始值為`null`，要對應表單元素的API，如`getElementById`獲取元素參照時，沒得到時會回傳`null`
+  // 1. 如果是會用在DOM元素上，會使用初始值為`null`
+  // 要對應API如`getElementById`獲取元素物件參照時沒得到時會回傳`null`
+  // 下面執行後 inputRef 會是一個物件 { current: null }
   const inputRef = useRef(null)
 
   return (
     <>
       <h2>input使用refs</h2>
+      {/* 2. 用類似屬性指定值的方式套用到要對應的DOM元素上 */}
       <input type="text" ref={inputRef} />
       <button
         onClick={() => {
+          // 3. 使用時使用inputRef.current
           alert(inputRef.current.value)
         }}
       >
@@ -24,7 +28,7 @@ export default function InputRefs() {
       </button>
       <button
         onClick={() => {
-          inputRef.current.blur()
+          document.getElementById('my-input').blur()
         }}
       >
         失焦(blur)
