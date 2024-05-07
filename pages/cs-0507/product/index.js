211044382518337230b0037c22e0574eaf32a8c7
Yun <159111847+XiaoYun3893z@users.noreply.github.com>
2024-05-07 13:52:05 +0800

    商品列表與詳細頁-路由說明

---
+++ pages/cs-0507/product/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Index() {
+  return (
+    <>
+      <h1>Index代表這資料夾的路由檔(根)</h1>
+    </>
+  )
+}
---
+++ pages/cs-0507/product/list.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function List() {
+  return (
+    <>
+      <h1>商品列表頁</h1>
+    </>
+  )
+}
